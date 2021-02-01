f6afa0948e5ba3fcd2ecafc0f592196bd96ea75a
claire847 <33422168+claire847@users.noreply.github.com>
2021-01-31 20:34:02 -0500

    Implemented priority queue -C

--- CIS479Project1.cpp
+++ CIS479Project1.cpp
@@ -4,9 +4,29 @@
 #include <queue>
 using namespace std;
 
+void printPuzzle(vector<vector<int>>, int, int);
+
+int currentLevel = 0;
+int expansionOrder = 1;
+
+struct Node
+{
+	vector<vector<int>> puzzle;
+	int fn;
+	int level;
+	int currentPathCost;
+
+	bool operator<(const Node &o) const
+	{		
+		return fn < o.fn || (fn == o.fn && level > o.level);
+	}
+};
+
+priority_queue <Node> q;
 
 //Gets Windy Manhattan Distance of a 3x3 2d puzzle array. Y is up and down, x is side to side. Use negs & abs values after getting value to determine amount due to wind
-int heuristicFunction(int p[][3])
+//int heuristicFunction(int p[][3])
+int heuristicFunction(vector<vector<int>> p)
 {
 	int num, x, y, total = 0;
 	
@@ -80,28 +100,31 @@ int heuristicFunction(int p[][3])
 //Get location of the 0(or -). Find it's coordinates based on y/x. Based on it's y/x, "if" statement to get the correct values that can move to it's spot.
 //With new puzzle from moved tile, send to heuristic function to get h distance. Add h distance to path cost(1,2,3 based on wind). Put nums as negative so you can use
 //the "top" function which grabs the largest value, to get the lowest value which is what we want. Do this until h is 0, ie puzzle solved.
-void priorityQueue(int p[][3])
+
+void priorityQueue(Node startingNode)
 {
-	int num, x, y, pTemp, pathCost = 0, val1 = 2147483000, val2 = 2147483000, val3 = 2147483000, val4 = 2147483000, hTemp, h = 2147483000;
-	priority_queue <int> q;
-	
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 3; j++)
-		    cout << p[i][j];
-	cout << endl;
-	h = heuristicFunction(p);
-	cout << pathCost << endl;
-	cout << h << endl;
+	Node currentNode = startingNode;
+	vector<vector<int>> p;
+	int num, x, y, pTemp, pathCost, fn1 = 2147483000, fn2 = 2147483000, fn3 = 2147483000, fn4 = 2147483000, hTemp, h = 2147483000;
+	vector<vector<int>> p1, p2, p3, p4;
+	Node n1,n2,n3,n4;
+
+	h = heuristicFunction(currentNode.puzzle);
 	
 	while(h != 0)
 	{
-		val1 = 2147483000;
-		val2 = 2147483000; 
-		val3 = 2147483000; 
-		val4 = 2147483000;
+		p = currentNode.puzzle;
+		pathCost = currentNode.currentPathCost;
+		fn1 = 2147483000;
+		fn2 = 2147483000; 
+		fn3 = 2147483000; 
+		fn4 = 2147483000;
 		h = 2147483000;
 		pTemp = pathCost;
+		p1.clear(); p2.clear(); p3.clear(); p4.clear();
+		n1 = {}; n2 = {}, n3 = {}, n4 = {};
 		
+		//determines where empty tile is
 		for(int i = 0; i < 3; i++)
 		{
 			for(int j = 0; j < 3; j++)
@@ -122,11 +145,14 @@ void priorityQueue(int p[][3])
 			p[y+1][x] = 0;
 			
 			h = heuristicFunction(p);
-			pTemp += 3;
-			val1 = h + pTemp;
+			pTemp += 1;
+			fn1 = h + pTemp;
+			p1 = p;
 			
 			p[y+1][x] = p[y][x];
 			p[y][x] = 0;
+
+			pTemp = pathCost;
 		}
 		
 		if((x+1) < 3)
@@ -136,10 +162,13 @@ void priorityQueue(int p[][3])
 			
 			h = heuristicFunction(p);
 			pTemp += 2;
-			val2 = h + pTemp;
+			fn2 = h + pTemp;
+			p2 = p;
 			
 			p[y][x+1] = p[y][x];
 			p[y][x] = 0;
+
+			pTemp = pathCost;
 		}
 		
 		if((x-1) >= 0)
@@ -149,10 +178,13 @@ void priorityQueue(int p[][3])
 			
 			h = heuristicFunction(p);
 			pTemp += 2;
-			val3 = h + pTemp;
+			fn3 = h + pTemp;
+			p3 = p;
 		
 			p[y][x-1] = p[y][x];
 			p[y][x] = 0;
+
+			pTemp = pathCost;
 		}
 		
 		if((y-1) >= 0)
@@ -161,66 +193,60 @@ void priorityQueue(int p[][3])
 			p[y-1][x] = 0;
 			
 			h = heuristicFunction(p);
-			pTemp +=1;
-			val4 = h + pTemp;
+			pTemp +=3;
+			fn4 = h + pTemp;
+			p4 = p;
 			
 			p[y-1][x] = p[y][x];
 			p[y][x] = 0;
+
+			pTemp = pathCost;
 		}
+				
+		n1 = {p1, -fn1, currentNode.level+1, currentNode.currentPathCost+1};
+		n2 = {p2, -fn2, currentNode.level+1, currentNode.currentPathCost+2};
+		n3 = {p3, -fn3, currentNode.level+1, currentNode.currentPathCost+2};
+		n4 = {p4, -fn4, currentNode.level+1, currentNode.currentPathCost+3};
+		q.pop();
+		q.push(n1);
+		q.push(n2);
+		q.push(n3);
+		q.push(n4);
 		
-	    q.push(-val1);
-	    q.push(-val2);
-	    q.push(-val3);
-	    q.push(-val4);
-	
-		int prio = q.top();
-		prio *= -1;
-		q = priority_queue <int>(); 
-		
-		if(prio == val1)
-		{
-			p[y][x] = p[y+1][x];
-			p[y+1][x] = 0;
-			pathCost += 1;
-		}
-		else if(prio == val2)
-		{
-			p[y][x] = p[y][x+1];
-			p[y][x+1] = 0;
-			pathCost += 2;
-		}
-		else if(prio == val3)
-		{
-			p[y][x] = p[y][x-1];
-			p[y][x-1] = 0;
-			pathCost += 2;
-		}
-		else if(prio == val4)
-		{
-			p[y][x] = p[y-1][x];
-			p[y-1][x] = 0;
-			pathCost += 3;
-		}
-		
+		currentNode = q.top();
 		h = heuristicFunction(p);
 		
-		for (int i = 0; i < 3; i++)
-		    	for (int j = 0; j < 3; j++)
-		        	cout << p[i][j];
-		cout << endl;
-		cout << pathCost << endl;
-		cout << h << endl;
+		printPuzzle(p, pathCost, h);
+		expansionOrder++;
 	}
-	
-	//cout << pathCost;
+}
+
+void printPuzzle(vector<vector<int>> p, int pathCost, int h)
+{
+	cout << "----------";
+	for (int i = 0; i < 3; i++)
+	{
+		cout << endl;
+		for (int j = 0; j < 3; j++)
+		{
+			if(p[i][j] == 0)
+				cout << "- ";
+			else
+				cout << p[i][j] << " ";
+		}
+	}
+		cout << endl;
+		cout << pathCost << "  " << h << "\n #" << expansionOrder << endl;
 }
 
 int main()
 {
-	int puzzle[3][3] = {{2, 8, 3}, {6, 7, 4}, {1, 5, 0}};
-	//int puzzle[3][3] = {{2, 8, 3}, {6, 0, 4}, {1, 7, 5}};
+	vector<vector<int>> startingPuzzle {{2, 8, 3}, {6, 7, 4}, {1, 5, 0}};
+	Node startingNode = {startingPuzzle, 0, 0, 0};
 
-	priorityQueue(puzzle);
+	q.push(startingNode);
+
+	priorityQueue(startingNode);
 
 	return 0;
 }
